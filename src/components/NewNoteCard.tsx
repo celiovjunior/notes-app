108e64ebf6335716fc8a355587a0089235cb8b71
Célio Júnior <celio_cjr@hotmail.com>
2024-02-07 13:57:35 +1300

    feat: installed sonner to set up toast notify

--- src/components/NewNoteCard.tsx
+++ src/components/NewNoteCard.tsx
@@ -1,7 +1,30 @@
 import * as Dialog from '@radix-ui/react-dialog'
 import { X } from 'lucide-react'
+import { toast } from 'sonner'
+import { ChangeEvent, FormEvent, useState } from 'react'
 
 export function NewNoteCard() {
+  const [shouldShowOnBoarding, setShouldShowOnBoarding] = useState(true)
+  const [content, setContent] = useState('')
+
+  function handleStartEditor() {
+    setShouldShowOnBoarding(false)
+  }
+
+  function handleContentChanged(event: ChangeEvent<HTMLTextAreaElement>) {
+    setContent(event.target.value)
+    
+    if (event.target.value === '') {
+      setShouldShowOnBoarding(true)
+    }
+  }
+
+  function handleSaveNote(event: FormEvent) {
+    event.preventDefault()
+    console.log(content)
+    toast.success("Your note has been saved")
+  }
+
   return (
     <Dialog.Root>
       <Dialog.Trigger className='rounded-md flex flex-col bg-slate-700 p-5 text-left gap-3 outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400'>
@@ -17,21 +40,32 @@ export function NewNoteCard() {
             <X className='w-5 h-5' />
           </Dialog.Close>
 
-          <div className='flex flex-1 flex-col gap3 p-5'>
-            <span className='text-sm font-medium text-slate-200'>
-              Add a new note
-            </span>
-            <p className='text-sm leading-6 text-slate-300'>
-              Start <button className='font-medium text-lime-400 hover:underline'>recording an audio</button> or <button className='font-medium text-lime-400 hover:underline'>write some note</button>.
-            </p>
-          </div>
+          <form onSubmit={handleSaveNote} className='flex-1 flex flex-col'>
+            <div className='flex flex-1 flex-col gap3 p-5'>
+              <span className='text-sm font-medium text-slate-200'>
+                Add a new note
+              </span>
+              { shouldShowOnBoarding ? (
+                    <p className='text-sm leading-6 text-slate-300'>
+                    Start <button className='font-medium text-lime-400 hover:underline'>recording an audio</button> or <button onClick={handleStartEditor} className='font-medium text-lime-400 hover:underline'>write some note</button>.
+                    </p>
+              ) : (
+                <textarea
+                autoFocus 
+                className='text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none' 
+                onChange={handleContentChanged}
+                />
+              ) }
+            </div>
 
-          <button 
-          type='button'
-          className='w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500'
-          >
-            Save note
-          </button>
+            <button 
+            type='submit'
+            className='w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500'
+            >
+              Save note
+            </button>
+          </form>
+
         </Dialog.Content>
       </Dialog.Portal>
 
