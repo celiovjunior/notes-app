37b50d74e9d411e84c88043f52663ec92acd5676
Célio Júnior <celio_cjr@hotmail.com>
2024-02-10 15:15:10 +1300

    feat: front-end: change the styles of the button if the user is recording

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 import logo from './assets/logo.svg'
 import { NewNoteCard } from './components/NewNoteCard'
 import { NoteCard } from './components/NoteCard'
@@ -10,6 +10,7 @@ interface Note {
 }
 
 export function App() {
+  const [search, setSearch] = useState('')
   const [notes, setNotes] = useState<Note[]>(() => {
     const notesOnStorage = localStorage.getItem('notes')
     if (notesOnStorage) {
@@ -32,6 +33,15 @@ export function App() {
     localStorage.setItem('notes', JSON.stringify(notesArray))
   }
 
+  function handleSearch(event: ChangeEvent<HTMLInputElement>) {
+    const query = event.target.value
+    setSearch(query)
+  }
+
+  const filteredNotes = search !== '' 
+    ? notes.filter(note => note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
+    : notes
+
   return (
     <div className='mx-auto max-w-6xl my-12 space-y-6'>
       <img src={logo} alt="Next level week event logo" />
@@ -41,6 +51,7 @@ export function App() {
           type="text" 
           placeholder='Search your notes...'
           className='w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500'
+          onChange={handleSearch}
          />
       </form>
 
@@ -50,7 +61,7 @@ export function App() {
 
         <NewNoteCard onNoteCreated={onNoteCreated} />
 
-        {notes.map((note => {
+        {filteredNotes.map((note => {
           return <NoteCard key={note.id} date={note.date} content={note.content} />
         }))}
         
--- src/components/NewNoteCard.tsx
+++ src/components/NewNoteCard.tsx
@@ -9,6 +9,7 @@ interface NewNoteCardProps {
 
 export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
   const [shouldShowOnBoarding, setShouldShowOnBoarding] = useState(true)
+  const [isRecording, setIsRecording] = useState(false)
   const [content, setContent] = useState('')
 
   function handleStartEditor() {
@@ -34,6 +35,10 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
     toast.success("Your note has been saved")
   }
 
+  function handleStartRecording() {
+    setIsRecording(true);
+  }
+
   return (
     <Dialog.Root>
       <Dialog.Trigger className='rounded-md flex flex-col bg-slate-700 p-5 text-left gap-3 outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400'>
@@ -56,7 +61,7 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
               </span>
               { shouldShowOnBoarding ? (
                     <p className='text-sm leading-6 text-slate-300'>
-                    Start <button className='font-medium text-lime-400 hover:underline'>recording an audio</button> or <button onClick={handleStartEditor} className='font-medium text-lime-400 hover:underline'>write some note</button>.
+                    Start <button onClick={handleStartRecording} type='button' className='font-medium text-lime-400 hover:underline'>recording an audio</button> or <button type='button' onClick={handleStartEditor} className='font-medium text-lime-400 hover:underline'>write some note</button>.
                     </p>
               ) : (
                 <textarea
@@ -65,15 +70,25 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
                   onChange={handleContentChanged}
                   value={content}
                 />
-              ) }
+              )}
             </div>
 
-            <button 
-            type='submit'
-            className='w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500'
-            >
-              Save note
-            </button>
+            {isRecording ? (
+              <button 
+              type='submit'
+              className='w-full bg-slate-900 py-4 text-center text-sm text-slate-300 outline-none font-medium hover:text-slate-100'
+              >
+                Recording! (click to stop)
+              </button>
+            ) : (
+              <button 
+              type='submit'
+              className='w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500'
+              >
+                Save note
+              </button>
+            )}
+
           </form>
 
         </Dialog.Content>
