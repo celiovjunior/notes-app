ab919307e13c18b137c241642b66f9f5a5d8fb67
Célio Júnior <celio_cjr@hotmail.com>
2024-02-10 16:42:11 +1300

    feat: working in responsivity, used iphone 12 shape

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ export function App() {
     : notes
 
   return (
-    <div className='mx-auto max-w-6xl my-12 space-y-6'>
+    <div className='mx-auto max-w-6xl my-12 space-y-6 px-5'>
       <img src={logo} alt="Next level week event logo" />
 
       <form className='w-full'>
@@ -57,7 +57,7 @@ export function App() {
 
       <div className='h-px bg-slate-700' />
 
-      <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
+      <div className='grid grid-cols-1 lg:grid-cols-3 gap-6 auto-rows-[250px]'>
 
         <NewNoteCard onNoteCreated={onNoteCreated} />
 
